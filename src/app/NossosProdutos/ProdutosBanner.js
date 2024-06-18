3f7a91fa595117530baaf0b0ff65c97099f053b1
RafaelTaves <rafaeljosetaves@gmail.com>
2024-06-18 17:47:49 -0300

    Página nossos produtos - falta passar cor como props

--- src/app/NossosProdutos/ProdutosBanner.js
+++ src/app/NossosProdutos/ProdutosBanner.js
@@ -1,19 +1,46 @@
+import Link from "next/link";
 import React from "react";
 
-export default function ProdutosBanner () {
-    
+
+export default function ProdutosBanner (props) {
+
+    let bgColorClass = '';
+
+  switch (props.color) {
+    case 'red':
+      bgColorClass = '[#921004]';
+      break;
+    case 'light-blue':
+      bgColorClass = '[#009790]';
+      break;
+    case 'dark-blue':
+      bgColorClass = '[#00A4EE]';
+      break;
+    // Adicione mais cores conforme necessário
+    default:
+      bgColorClass = 'bg-gray-500'; // Cor padrão caso não haja correspondência
+      break;
+  }
+
     return(
         <>
-            <h2 className="h5Headline customPurple text-center md:text-left md:h4Headline mb-10">Nossos produtos:</h2>
-            <div className="flex flex-col md:flex-row items-center justify-between p-8 bg-[#921004] rounded-3xl">
+            <div className={`flex mx-6 md:mx-0 flex-col md:flex-row items-center justify-between p-8 bg-${bgColorClass} rounded-3xl mb-8`}>
                 <div className="md:w-1/2 text-left">
-                    <img className="mb-8" src="./imagens/logoInffelBranco.png"></img>
-                    <p className="body3 text-white underline mb-8"># FoodTech</p>
-                    <h4 className="h6Headline text-white md:h4Headline mb-4">Tornando os restaurantes um lugar incrível!</h4>
-                    <p className="body2 text-white md:body1">O Inffel está cheio de soluções inteligentes e intuitivas para você poder otimizarao máximo o seu food-service.</p>
+                    <img className="mb-8" src={props.srcLogo} ></img>
+                    <p className="body3 text-white underline mb-8">{props.subtitulo}</p>
+                    <h4 className="h6Headline text-white md:h4Headline mb-4">{props.titulo}</h4>
+                    <p className="body2 text-white md:body1">{props.p}</p>
+                    <Link href="">
+                    <button className={`mt-6 px-6 py-2 bg-white border rounded-full border-white text-${bgColorClass} flex items-center gap-2`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg>
+                        Ir para o site
+                    </button>
+                    </Link>
                 </div>
-                <div className="mt-8 md:mt-0 md:w-1/2 md:ml-auto flex justify-end relative ">
-                
+                <div className="mt-8 md:mt-0 md:w-1/4 md:ml-auto flex justify-end relative md:me-40">
+                    <img src={props.srcForma}></img>
                 </div>
             </div>
         </>    
--- src/app/NossosProdutos/page.js
+++ src/app/NossosProdutos/page.js
@@ -5,6 +5,7 @@ import TextImg from '../GlobalComponents/Text&Img'
 import ProdutosBanner from './ProdutosBanner'
 
 export default function NossosProdutos () {
+    
     return(
         <>
         <div id="NossosProdutos" className="App mx-auto max-w-screen-2xl">
@@ -17,7 +18,23 @@ export default function NossosProdutos () {
                 imgSrc={"./imagens/imgNossosProdutos.png"}
                 imgAlt="Medalha"
             />
-            <ProdutosBanner />
+            <h2 className="h5Headline customPurple text-center md:text-left md:h4Headline mb-10">Nossos produtos:</h2>
+            <ProdutosBanner 
+            color= "red"
+            srcLogo="./imagens/logoInffelBranco.png"
+            subtitulo="# FoodTech"
+            titulo="Tornando os restaurantes um lugar incrível!"
+            p="O Inffel está cheio de soluções inteligentes e intuitivas para você poder otimizarão máximo o seu food-service."
+            srcForma="./imagens/imgInffelBanner.png"
+            />
+            <ProdutosBanner 
+            color= "light-blue"
+            srcLogo="./imagens/logoTotal2.png"
+            subtitulo="# LawTech"
+            titulo="Plataforma online que agiliza a lavratura da escritura e procuração!"
+            p="A Total Sistema simplifica a intermediação entre clientes e cartórios por meio da tecnologia para você poder otimizar ao máximo o seu cartório."
+            srcForma="./imagens/meioCirculo.png"
+            />
         </div>
         <Footer />
         </>
